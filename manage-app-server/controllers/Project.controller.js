2b66ff19fce1cdc96b2981f83e5e8154a3a7c97f
Hoàng Dương Vũ <hdvu98@gmail.com>
2020-04-21 17:55:36 +0700

    update project -- api

--- manage-app-server/controllers/Project.controller.js
+++ manage-app-server/controllers/Project.controller.js
@@ -60,6 +60,31 @@ module.exports = {
       })
       .catch((err) => res.status(400).send({ message: err.message }));
   },
-  updateProject: (req, res, next) => {},
+  updateProject: (req, res, next) => {
+    var { project_name, description } = req.body;
+    var project = {};
+    if (project_name) {
+      if (!projectNameValidation(project_name))
+        return res.status(400).send({
+          message: 'project_name format is invalid',
+        });
+      project = { ...project, project_name: project_name };
+    }
+    if (description) {
+      if (description.length > 1000)
+        return res.status(400).send({
+          message: 'Description must have fewer than 1000 characters',
+        });
+      project = { ...project, description: description };
+    }
+
+    Project.update({ _id: req.params.id }, project)
+      .then((data) => {
+        if (!data)
+          return res.status(400).send({ message: 'the project is not exits' });
+        return res.send(data);
+      })
+      .catch((err) => res.status(400).send({ message: err.message }));
+  },
   remove: (req, res, next) => {},
 };

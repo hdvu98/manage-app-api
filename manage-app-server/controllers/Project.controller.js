31a04ed77c3bfb618e6c485e78bc2897909359c3
Hoàng Dương Vũ <hdvu98@gmail.com>
2020-04-21 16:56:47 +0700

    create project feature api

---
+++ manage-app-server/controllers/Project.controller.js
@@ -0,0 +1,29 @@
+const moment = require('moment');
+const Project = require('./../models/Project.model');
+const { projectNameValidation } = require('./../utils/helpers');
+module.exports = {
+  createProject: (req, res, next) => {
+    var { project_name, description } = req.body;
+    if (!project_name) {
+      return res.status(400).send({
+        message: 'project_name field is required',
+      });
+    }
+    if (!projectNameValidation(project_name))
+      return res.status(400).send({
+        message: 'project_name format is invalid',
+      });
+    var project = { project_name, description };
+    Project.create(project)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  },
+  getProjects: (req, res, next) => {},
+  getProjectByID: (req, res, next) => {},
+  updateProject: (req, res, next) => {},
+  remove: (req, res, next) => {},
+};
---
+++ manage-app-server/routes/Project.route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+var router = express.Router();
+var Project = require('../controllers/Project.controller');
+
+router.post('/create', Project.createProject);
+router.get('/get', Project.getProjects);
+router.get('/get/:id', Project.getProjectByID);
+router.put('/update/:id', Project.updateProject);
+router.delete('/remove/:id', Project.remove);
+module.exports = router;

31d9ab1c30d576ca80feec4bf6fc6355e24bbb77
Hoàng Dương Vũ <hdvu98@gmail.com>
2020-04-21 17:26:32 +0700

    show project details api

--- manage-app-server/controllers/Project.controller.js
+++ manage-app-server/controllers/Project.controller.js
@@ -1,5 +1,7 @@
 const moment = require('moment');
 const Project = require('./../models/Project.model');
+const Member = require('./../models/Member.model');
+
 const { projectNameValidation } = require('./../utils/helpers');
 module.exports = {
   createProject: (req, res, next) => {
@@ -27,7 +29,37 @@ module.exports = {
       .then((data) => res.send(data))
       .catch((err) => res.status(400).send({ message: err.message }));
   },
-  getProjectByID: (req, res, next) => {},
+  getProjectByID: (req, res, next) => {
+    Project.get({ _id: req.params.id })
+      .then(async (data) => {
+        if (data.length > 0) {
+          var project = data[0];
+          const { assignees } = project;
+          var assigneesInfo = [];
+          if (assignees && assignees.length > 0) {
+            assigneesInfo = await Promise.all(
+              assignees.map(async (item) => {
+                let info;
+                try {
+                  info = await Member.get({ _id: item });
+                  if (info.length > 0) return info[0];
+                  return null;
+                } catch (err) {
+                  return null;
+                }
+              })
+            );
+            assigneesInfo = assigneesInfo.filter(function (el) {
+              return el != null;
+            });
+          }
+          project.assignees = assigneesInfo;
+          return res.send(project);
+        }
+        return res.send(data);
+      })
+      .catch((err) => res.status(400).send({ message: err.message }));
+  },
   updateProject: (req, res, next) => {},
   remove: (req, res, next) => {},
 };
